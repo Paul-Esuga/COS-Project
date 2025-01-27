b4d35fe5d0d3de762a7d757395e7d19d7e47c06f
Majiyebo-Esuga <93590248+Paul-Esuga@users.noreply.github.com>
2025-01-27 20:11:33 +0100

    Update Authentication.cpp

--- Authentication.cpp
+++ Authentication.cpp
@@ -8,43 +8,37 @@
 using namespace std;
 
 void Start();
-bool Authenticate(string user, string pass);
+bool Authenticate(string u, string p);
 
-
-class Users { //class to save user credentials
-    public: 
-        string usern;
-        string pass;
-    public: 
-        Users();
-        Users(string user, string passw) {usern = user; pass = passw;};
-        bool authenticate(string user, string passw); 
-}; 
-
-bool Users::authenticate(string user, string passw) {
-    return ((user == Users::usern) && (passw == Users::pass));
-};
-
-Users userarr[6] = { //array stores the user classes
-    Users("Timothy", "Tim123"),
-    Users("Gregory", "BlockChainz3"),
-    Users("Emmanuel", "H@rtL1v3d"),
-    Users("Maji", "1234"),
-    Users("Yaya", "He15"),
-    Users("Moses", "Welcome2PAU")
-};
-
-bool Authenticate(string user, string pass) { //function compares user input to preset credentials
-    bool x = false;
-    for (int i = 0; i < 6; i++) {
-        if (userarr[i].authenticate(user, pass)) {
-            x = true;
+bool Authenticate(string u, string p) {
+    bool Us = false, Pass = false;
+    ifstream file("data.txt"); // Open the file
+    string line;
+    int count1 = 0;
+    if (file.is_open()) {
+        while (getline(file, line)) { // Read each line from the file
+            stringstream ss(line);    // Create a stringstream for the line
+            string value;
+            // Extract and process values separated by the delimiter
+            while (getline(ss, value, ',')) {
+                if (count1 < 1) {
+                    if (u == value) { Us = true; }
+                }
+                else {
+                    if (p == value) { Pass = true; }
+                }
+                count1++;
+            }
+            count1 = 0;
         }
+        file.close(); // Close the file
+    } else {
+        cout << "Unable to open file." << endl;
     }
-    return x;
-};
+    return (Us && Pass);
+}
 
-void Start() { // main function for the program, sets the authentication process in motion
+void Start() {
     string US, PW;
     cout << "Kindly input your username: ";
     cin >> US;
@@ -60,40 +54,11 @@ void Start() { // main function for the program, sets the authentication process
     }
     
     if (Authenticate(US, PW)) {
-        //Test();
         cout << "Welcome in" << endl;
     }
 };
 
-string arr[3][2];
-string arr2[9];
-
 int main() {
     Start();
-    ifstream file("data.txt"); // Open the file
-    string line;
-    int count1 = 0;
-    if (file.is_open()) {
-        while (getline(file, line)) { // Read each line from the file
-            stringstream ss(line);    // Create a stringstream for the line
-            string value;
-
-            // Extract and process values separated by the delimiter
-            while (getline(ss, value, ',')) {
-
-                cout << value << " "; // Print the value
-                arr2[count1] = value;
-                count1++;
-            }
-            cout << endl; // Newline after processing each line
-        }
-        file.close(); // Close the file
-    } else {
-        cout << "Unable to open file." << endl;
-    }
-    for (int i = 0; i < 9; i++) {
-        cout << arr2[i] << "\t";
-    }
-    
     return 0;
 }
